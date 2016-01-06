9b908af62d54f96780638f46702275904253d9fe
averainy <zyongxin2916@gmail.com>
2016-01-06 21:28:34 +0800

    add

--- main.py
+++ main.py
@@ -7,12 +7,28 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
+from config import sql
+
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
+
+class SingleHandler(tornado.web.RequestHandler):
+    def get(self):
+        single_id = self.get_argument('id')
+        db=sql.DB()
+        info=db.get_article(int(single_id))
+        title=info[1]
+        content=info[2]
+        tag=info[4]
+        update_time=info[5].strftime('%Y-%m-%d %H:%M:%S')
+        self.render('single.html',title=title,
+                content=content,
+                tag=tag,
+                update_time=update_time)
 
 class PoemPageHandler(tornado.web.RequestHandler):
     def post(self):
@@ -26,8 +42,10 @@ class PoemPageHandler(tornado.web.RequestHandler):
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     app = tornado.web.Application(
-        handlers=[(r'/', IndexHandler), (r'/poem', PoemPageHandler)],
-        template_path=os.path.join(os.path.dirname(__file__), "templates")
+        handlers=[(r'/', SingleHandler), (r'/poem', PoemPageHandler)],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        debug=True
     )
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
